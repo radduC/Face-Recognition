ab71d04ee96dda68438213adc35492f37c69adf4
radu costache <sir.raddu@gmail.com>
2020-05-28 20:37:08 +0300

    first commit

---
+++ precache-manifest.51de9a95fd7b8435098c0e13dc001050.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe07178dcd21b8bf7216aef7bed2709",
+    "url": "/radduC/Face-Recognition/index.html"
+  },
+  {
+    "revision": "e75c9a211bb61d5c4b83",
+    "url": "/radduC/Face-Recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "795ece36b46a0e3aed1d",
+    "url": "/radduC/Face-Recognition/static/css/main.f1017724.chunk.css"
+  },
+  {
+    "revision": "e75c9a211bb61d5c4b83",
+    "url": "/radduC/Face-Recognition/static/js/2.359e01c9.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/radduC/Face-Recognition/static/js/2.359e01c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795ece36b46a0e3aed1d",
+    "url": "/radduC/Face-Recognition/static/js/main.8cd319b5.chunk.js"
+  },
+  {
+    "revision": "cb15f437d042f4f8e2a7",
+    "url": "/radduC/Face-Recognition/static/js/runtime-main.63552712.js"
+  }
+]);
